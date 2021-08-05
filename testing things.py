74c7417c6bae3b17a27ffbbdc750c492f4b1fdfb
EbrahimKaram <ebrahim_karam@hotmail.com>
2021-08-04 21:03:47 -0400

    Added seperate pickles there is a pickle file for production server and test server

--- testing things.py
+++ testing things.py
@@ -2,7 +2,12 @@ import pickle
 
 
 if __name__ == '__main__':
-    messages_to_monitor = [870745251017535508,870745212899717190,870745192704147507,870744923404656740,870745185364099143]
-    message_pickle = 'message.pickle'
+    # messages_to_monitor = [870745251017535508, 870745212899717190,
+    #                        870745192704147507, 870744923404656740, 870745185364099143]  # test
+    messages_to_monitor = [871441078459465768, 871441084620890152,
+                           871441092992708658, 871441104225046569, 870745251017535508]  # production
+
+    # message_pickle = 'messages_test.pickle'  # test
+    message_pickle = 'messages_production.pickle'  # production
     with open(message_pickle, 'wb') as f:
         pickle.dump(messages_to_monitor, f)
